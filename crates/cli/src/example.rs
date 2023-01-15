3d22e4ed6cf0bbce08a5bb7de65b9f1780db01c1
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-01-15 17:03:03 -0600

    feat: example from ascent docs

---
+++ crates/cli/src/example.rs
@@ -0,0 +1,23 @@
+#![allow(clippy::clone_on_copy)]
+#![allow(clippy::let_unit_value)]
+use ascent::ascent;
+
+ascent! {
+    relation edge(i32, i32);
+    relation path(i32, i32);
+
+    path(x, y) <-- edge(x, y);
+    path(x, z) <-- edge(x, y), path(y, z);
+}
+
+pub (crate) fn run(edges: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    // let mut prog = AscentProgram::default();
+    // prog.edge = vec![(1, 2), (2, 3)];
+    let mut prog = AscentProgram {
+        edge: edges,
+        ..Default::default()
+    };
+    prog.run();
+
+    prog.path
+}
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,7 @@
+mod example;
+use example::run as example_run;
+
 fn main() {
-    println!("Hello, world!");
+    let connected = example_run(vec![(1, 2), (2, 3)]);
+    println!("Connected: {connected:?}");
 }
