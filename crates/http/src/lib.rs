b14fefdff9d5a90a85e0b36e4b60e8d28c5a0bb5
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-03-27 22:33:09 -0500

    feat: Serve site contents
    The server configuration now expects a `static_path` which will point to
    the frontend. This is used as a fallback service, so if no other routes
    can be matched the server will attempt to serve files.
    
    The `ServeDir` service has been configured to use a middleware that will
    write extra dom nodes to it. These nodes will be under a parent node
    with id = "injected-config". The intent is that the server will put any
    variables that it wishes to configure at runtime into them. For example,
    the auth_url variable comes from an environment variable to the server,
    and it will pass that to any frontends.
    
    The primary purpose of modifying HTML is so that we can support
    configurability in both scenarios which the site is running: local
    development via astro (which has direct access to environment variables
    in javascript)
    and with the axum api, from which astro does not know how to retrieve
    values. A small configuration loading system will need to be added to
    facilitate this.

--- crates/http/src/lib.rs
+++ crates/http/src/lib.rs
@@ -1,10 +1,20 @@
-#[allow(unused_imports)]
 use axum::{
-    routing::{get, post},
+    body::{self, BoxBody, HttpBody},
+    extract::State,
+    http::{Request, StatusCode},
+    middleware::{self, Next},
+    response::{Html, IntoResponse, Response},
+    routing::{get, get_service},
     Router,
 };
+use html_editor::{
+    operation::{Editable, Htmlifiable, Selector},
+    Node,
+};
 use lockpad_auth::PublicKey;
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr, path::PathBuf};
+use tower::ServiceBuilder;
+use tower_http::{services::ServeDir, ServiceBuilderExt};
 
 pub mod error;
 
@@ -14,11 +24,17 @@ pub struct Server {
     addr: SocketAddr,
 
     public_keys: Vec<PublicKey>,
+    static_path: PathBuf,
 }
 
 #[derive(Clone)]
 pub struct ServerState {
     pub public_key: PublicKey,
+}
+
+#[derive(Clone, Debug)]
+pub struct FrontendState {
+    pub variables: HashMap<String, String>,
 }
 
 impl AsRef<PublicKey> for ServerState {
@@ -38,23 +54,103 @@ impl Server {
         let public_key = self.public_keys[0].clone();
         let state = ServerState { public_key };
 
-        let app = Router::new()
-            .route("/", get(root))
-            .with_state(state)
-            .layer(cors);
+        // TODO: populate frontend_state with necessary variables
+        let mut frontend_state = FrontendState {
+            variables: HashMap::new(),
+        };
+        frontend_state
+            .variables
+            .insert("auth_url".to_string(), "http://localhost:3000".to_string());
 
-        tracing::info!("Listening on {0}", self.addr);
+        // serve all files from the static directory
+        // if the file contains html, it will be edited to include runtime environment variables
+        // these may be loaded by the client-side javascript
+        let serve_dir = ServeDir::new(self.static_path);
+        let serve_service = get_service(serve_dir).handle_error(handle_error).layer(
+            ServiceBuilder::new().map_request_body(body::boxed).layer(
+                middleware::from_fn_with_state(frontend_state, inject_variables_into_html),
+            ),
+        );
+
+        let app = Router::new()
+            .with_state(state)
+            .route("/", get(root))
+            .layer(cors)
+            .fallback_service(serve_service);
+
+        tracing::debug!("listening on {}", &self.addr);
         axum::Server::bind(&self.addr)
             .serve(app.into_make_service())
-            .await?;
+            .await
+            .unwrap();
 
         Ok(())
     }
 }
 
+async fn inject_variables_into_html(
+    State(state): State<FrontendState>,
+    request: Request<BoxBody>,
+    next: Next<BoxBody>,
+) -> std::result::Result<impl IntoResponse, Response> {
+    let response = next.run(request).await;
+
+    let (mut parts, body) = response.into_parts();
+
+    let is_html = parts
+        .headers
+        .get(hyper::header::CONTENT_TYPE)
+        .map(|value| value == "text/html")
+        .unwrap_or(false);
+
+    let body = match is_html {
+        true => {
+            // Extract the body and parse it into a dom
+            let bytes = hyper::body::to_bytes(body).await.unwrap();
+            let html = String::from_utf8(bytes.to_vec()).unwrap();
+            let mut dom = html_editor::parse(&html).unwrap();
+
+            // Add the variables to the dom
+            let nodes = state
+                .variables
+                .iter()
+                .map(|(key, value)| {
+                    Node::new_element(
+                        "div",
+                        vec![(&format!("data-config-{}", key), value)],
+                        vec![],
+                    )
+                })
+                .collect::<Vec<_>>();
+            let parent = Node::new_element("div", vec![("id", "injected-config")], nodes);
+            dom.insert_to(&Selector::from("body"), parent);
+
+            // Create a new response body
+            let new_html = dom.html();
+            let bytes = new_html.as_bytes().to_vec();
+            let body = body::boxed(axum::body::Full::from(bytes));
+
+            // update the content length header to match the new body
+            let content_length = body.size_hint().exact().unwrap();
+            parts.headers.insert(
+                hyper::header::CONTENT_LENGTH,
+                hyper::header::HeaderValue::from(content_length),
+            );
+
+            body
+        }
+        false => body,
+    };
+
+    let response = Response::from_parts(parts, body);
+
+    Ok(response)
+}
+
 pub struct Builder {
     addr: Option<SocketAddr>,
     public_keys: Option<Vec<PublicKey>>,
+    static_path: Option<PathBuf>,
 }
 
 impl Builder {
@@ -62,6 +158,7 @@ impl Builder {
         Self {
             addr: None,
             public_keys: None,
+            static_path: None,
         }
     }
 
@@ -75,11 +172,21 @@ impl Builder {
         self
     }
 
+    pub fn static_path(mut self, static_path: PathBuf) -> Self {
+        self.static_path = Some(static_path);
+        self
+    }
+
     pub fn build(self) -> Result<Server> {
         let addr = self.addr.ok_or(error::Error::ServerBuilder)?;
         let public_keys = self.public_keys.ok_or(error::Error::ServerBuilder)?;
+        let static_path = self.static_path.ok_or(error::Error::ServerBuilder)?;
 
-        Ok(Server { addr, public_keys })
+        Ok(Server {
+            addr,
+            public_keys,
+            static_path,
+        })
     }
 }
 
@@ -88,10 +195,15 @@ impl Default for Builder {
         Self {
             addr: Some(SocketAddr::from(([0, 0, 0, 0], 3000))),
             public_keys: None,
+            static_path: None,
         }
     }
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn root() -> Html<String> {
+    Html("<h1>Hello, world!</h1>".to_string())
+}
+
+async fn handle_error(_err: std::io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
 }
