484746a1baf1c81dc77b1a6f7947ebcef896d03c
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-09-16 19:17:44 -0500

    feat: introduce AppState
    A simple shared state for recipes so that they can be displayed and
    updated in different places. This may need to be split into further
    structs but there is no point in doing so yet.

--- crates/ui/src/routing.rs
+++ crates/ui/src/routing.rs
@@ -1,4 +1,4 @@
-use crate::{components::Recipe, util};
+use crate::{components::Recipe, state::AppState, util};
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
 
@@ -6,18 +6,20 @@ use dioxus_router::prelude::*;
 pub(crate) enum Route {
     #[route("/")]
     Index {},
-    /*
     #[route("/app")]
     AppIndex {},
     #[route("/app/recipes")]
     AppRecipes {},
-    */
 }
 
 #[allow(non_snake_case)]
 pub(crate) fn Index(cx: Scope) -> Element {
     cx.render(rsx! {
         div { "hello, wasm!" }
+        Link {
+            to: Route::AppRecipes {},
+            "recipes"
+        }
         button {
             onclick: |_| async move {
                 let filename = "test.txt";
@@ -30,5 +32,26 @@ pub(crate) fn Index(cx: Scope) -> Element {
             name: "pizza dough".to_string(),
             ingredients: vec!["flour".to_string(), "water".to_string(), "salt".to_string(), "yeast".to_string()],
         }
+    })
+}
+
+#[allow(non_snake_case)]
+pub(crate) fn AppIndex(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div { "app index" }
+    })
+}
+
+#[allow(non_snake_case)]
+pub(crate) fn AppRecipes(cx: Scope) -> Element {
+    let app_state = use_shared_state::<AppState>(cx).unwrap();
+
+    cx.render(rsx! {
+        app_state.read().recipes.iter().map(|recipe| rsx! {
+            Recipe {
+                name: recipe.name.clone(),
+                ingredients: recipe.ingredients.iter().map(|ingredient| ingredient.name.clone()).collect(),
+            }
+        })
     })
 }
