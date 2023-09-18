1f5fb57ce15f4420ca7d65c586818b45250a6664
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-09-17 22:42:28 -0500

    feat: navigation component

--- crates/ui/src/routing.rs
+++ crates/ui/src/routing.rs
@@ -9,6 +9,7 @@ use dioxus_router::prelude::*;
 
 #[derive(Debug, Clone, Routable)]
 pub(crate) enum Route {
+    #[layout(Nav)]
     #[route("/")]
     Index {},
     #[route("/app")]
@@ -18,13 +19,56 @@ pub(crate) enum Route {
 }
 
 #[allow(non_snake_case)]
+pub(crate) fn Nav(cx: Scope) -> Element {
+    let style_contents = r#"
+        .navlink {
+            margin: 0 1rem;
+
+            &.active {
+                color: red;
+                font-weight: bold;
+            }
+        }
+
+        .navlink:hover {
+            cursor: pointer;
+        }
+
+        .nav {
+            display: flex;
+            flex-direction: row;
+            border: 1px solid black;
+            padding: 1rem;
+        }
+    "#;
+    render! {
+        style {
+            style_contents
+        }
+        nav {
+            class: "nav",
+            Link {
+                active_class: "active",
+                class: "navlink",
+                to: Route::Index {},
+                "home"
+            }
+            Link {
+                active_class: "active",
+                class: "navlink",
+                to: Route::AppRecipes {},
+                "recipes"
+            }
+        }
+
+        Outlet::<Route> { }
+    }
+}
+
+#[allow(non_snake_case)]
 pub(crate) fn Index(cx: Scope) -> Element {
     cx.render(rsx! {
         div { "hello, wasm!" }
-        Link {
-            to: Route::AppRecipes {},
-            "recipes"
-        }
         button {
             onclick: |_| async move {
                 let filename = "test.txt";
