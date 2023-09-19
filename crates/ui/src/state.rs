1d29ca92596095a09f867b2216a39f3540a877bc
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-09-18 21:41:21 -0500

    feat(ui): create ingredients
    This implements the same style of form as for recipes, but this time for
    ingredients.

--- crates/ui/src/state.rs
+++ crates/ui/src/state.rs
@@ -6,3 +6,21 @@ pub(crate) struct AppState {
     pub recipes: Vec<Recipe>,
     pub ingredients: Vec<Ingredient>,
 }
+
+impl AppState {
+    pub fn add_recipe(&mut self, recipe: Recipe) {
+        self.recipes.push(recipe);
+    }
+
+    pub fn add_ingredient(&mut self, ingredient: Ingredient) {
+        self.ingredients.push(ingredient);
+    }
+
+    pub fn remove_recipe(&mut self, name: &str) {
+        self.recipes.retain(|r| r.name != name);
+    }
+
+    pub fn remove_ingredient(&mut self, name: &str) {
+        self.ingredients.retain(|i| i.name != name);
+    }
+}
