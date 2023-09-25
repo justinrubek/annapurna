9d2e8d3d8db322797de4eca1fdd6bc3d30a4212c
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-09-24 21:47:51 -0500

    feat: initialize service-worker crate
    This is a barebones service worker that doesn't do much other than
    some logging. The intents is to make this feature-compatible with the
    existing serviceworker that is written in JavaScript.

---
+++ crates/service-worker/src/lib.rs
@@ -0,0 +1,90 @@
+use wasm_bindgen::prelude::*;
+use web_sys::{console, ServiceWorkerGlobalScope};
+
+pub mod error;
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+fn init_worker() -> std::result::Result<(), JsValue> {
+    let global = js_sys::global();
+
+    if let Ok(true) = js_sys::Reflect::has(&global, &JsValue::from_str("ServiceWorkerGlobalScope"))
+    {
+        console::log_1(&JsValue::from_str("in service worker"));
+        // we're in a service worker, so we can cast the global to a ServiceWorkerGlobalScope
+        let global = global.unchecked_into::<ServiceWorkerGlobalScope>();
+
+        // Force immediate activation
+        let on_install = on_install(&global)?;
+        let on_activate = on_activate(&global)?;
+        global.set_oninstall(Some(on_install.as_ref().unchecked_ref()));
+        global.set_onactivate(Some(on_activate.as_ref().unchecked_ref()));
+
+        // register all the other callbacks
+        let on_message = on_message(&global)?;
+        let on_fetch = on_fetch(&global)?;
+        global.set_onmessage(Some(on_message.as_ref().unchecked_ref()));
+        global.set_onfetch(Some(on_fetch.as_ref().unchecked_ref()));
+
+        // Ensure that the closures are not dropped before the service worker is terminated
+        // This is technically a memory leak, but I'm not sure that it matters in this case
+        on_install.forget();
+        on_activate.forget();
+        on_message.forget();
+    } else {
+        console::log_1(&JsValue::from_str("not in service worker"));
+        return Err(error::Error::NotInServiceWorker.into());
+    }
+
+    Ok(())
+}
+
+fn on_install(
+    global: &ServiceWorkerGlobalScope,
+) -> std::result::Result<Closure<dyn FnMut(web_sys::ExtendableEvent)>, JsValue> {
+    let skip_waiting = global.skip_waiting()?;
+    Ok(Closure::wrap(
+        Box::new(move |event: web_sys::ExtendableEvent| {
+            event.wait_until(&skip_waiting).unwrap();
+        }) as Box<dyn FnMut(_)>,
+    ))
+}
+
+fn on_activate(
+    global: &ServiceWorkerGlobalScope,
+) -> std::result::Result<Closure<dyn FnMut(web_sys::ExtendableEvent)>, JsValue> {
+    let clients = global.clients();
+    Ok(Closure::wrap(
+        Box::new(move |event: web_sys::ExtendableEvent| {
+            event.wait_until(&clients.claim()).unwrap();
+        }) as Box<dyn FnMut(_)>,
+    ))
+}
+
+/// Displays a message in the console when a message is received from the client
+fn on_message(
+    _global: &ServiceWorkerGlobalScope,
+) -> std::result::Result<Closure<dyn FnMut(web_sys::ExtendableMessageEvent)>, JsValue> {
+    Ok(Closure::wrap(
+        Box::new(move |event: web_sys::ExtendableMessageEvent| {
+            console::log_2(&JsValue::from_str("sw msg:"), &event.data());
+        }) as Box<dyn FnMut(_)>,
+    ))
+}
+
+/// Intercepts and potentially modifies fetch requests that are made by the client
+fn on_fetch(
+    _global: &ServiceWorkerGlobalScope,
+) -> std::result::Result<Closure<dyn FnMut(web_sys::FetchEvent)>, JsValue> {
+    Ok(Closure::wrap(Box::new(move |event: web_sys::FetchEvent| {
+        let request = event.request();
+        console::log_2(&JsValue::from_str("sw fetch:"), &request);
+
+        // Peroform the actual fetch
+        let window = web_sys::window().unwrap();
+        let response = window.fetch_with_request(&request);
+
+        // ensure
+        event.respond_with(&response).unwrap();
+    }) as Box<dyn FnMut(_)>))
+}
