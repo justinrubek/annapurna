9f1d5557e1b9e7331fddd48ad7897e69e884d5ba
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-03-03 20:16:55 -0600

    ci(pre-commit): add sqlx prepare hook

--- flake-parts/pre-commit.nix
+++ flake-parts/pre-commit.nix
@@ -12,6 +12,14 @@
         src = ../.;
         hooks = {
           treefmt.enable = true;
+
+          sql-prepare = {
+            enable = true;
+            entry = "cargo sqlx prepare --workspace";
+            # add `--check` to check only. Without it the file will be updated when the hook is run
+            # entry = "cargo sqlx prepare --workspace --check";
+            pass_filenames = false;
+          };
         };
 
         settings.treefmt.package = self'.packages.treefmt;
